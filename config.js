d3701e0146fb818e539d91a5306d719c318d25a4
MrodAnd <165328870+MrodAnd@users.noreply.github.com>
2024-05-14 10:33:37 -0400

    new changes

--- config.js
+++ config.js
@@ -10,8 +10,8 @@ var config = {
     theme: 'dark',
     use3dTerrain: true, //set true for enabling 3D maps.
     auto: false,
-    title: 'Bikes and Trucks in NYC',
-    subtitle: 'Truck routes can be dangerous places for bikers. However, truck routes are essential for the city and allow for the movement of goods. What is incredibly dangerous is when GPS apps direct cyclists to use streets with trucks which occurs frequently for streets that have "shallows."',
+    title: 'Unprotected Bike Paths on Truck Routes in NYC',
+    subtitle: 'Truck routes can be dangerous places for bikers. However, truck routes are essential for the city and allow for the movement of goods. Often times, GPS apps direct cyclists to use truck route streets that have unprotected bike lanes or "sharrows."',
     byline: 'By Marcos Rodriguez Anderson',
     footer: 'Source: source citations, etc. <br> Created using <a href="https://github.com/MrodAnd/Final-Project" target="_blank">Mapbox Storytelling</a> template.',
     chapters: [
@@ -19,9 +19,9 @@ var config = {
             id: 'slug-style-id',
             alignment: 'left',
             hidden: false,
-            title: 'Display Title',
-            image: 'images/bikeandtruck.jpg',
-            description: ' Over 50% of fatal bike and pedestrian crashes happened on truck routes even though they make up just 13% of city roads. Truck Routes are important and are necessary in the city.',
+            title: 'Trucks Routes are Essential',
+            image: 'images/truckmovement.jpg',
+            description: 'Truck Routes are necessary for the city. Nevertheless, over 50% of fatal bike and pedestrian crashes happened on truck routes even though they make up just 13% of city roads.',
             location: {
                 center: [-73.98486, 40.74901],
                 zoom: 8.5,
@@ -50,9 +50,9 @@ var config = {
             id: 'second-identifier',
             alignment: 'right',
             hidden: false,
-            title: 'Streets that have Truck Routes and Bike Paths',
-            image: 'images/truckinbikelane.jpg',
-            description: 'Some Streets have both Truck Routes and Bike Lanes. These streets can be shown by the orange line. Truck route that have protected bike lanes have shown not be a serious issue for cyclist safetly. Truck routes that share the street with unprotected bike lanes and shallows is a different story.',
+            title: 'Streets with Protected Bike Lanes and Truck Routes are not the Problem',
+            image: 'images/protectlane.jpg',
+            description: 'Some streets have both truck routes and bike lanes which is shown by the orange line. Truck routes that have protected bike lanes have shown not be a serious issue for cyclist safetly but truck routes that share the street with unprotected bike lanes and sharrows is a different story.',
             location: {
                 center: [-73.91737, 40.71908],
                 zoom: 12.5,
@@ -75,9 +75,9 @@ var config = {
             id: 'third-identifier',
             alignment: 'left',
             hidden: false,
-            title: 'Bike Lanes',
+            title: 'Unprotected Bike Lanes can be an Issue',
             image: 'images/death1.jpg',
-            description: 'Overall, trucks were involved in half of last year’s 30 bike fatalities. Over 1/3rd of cyclist accidents occured on bike marked streets. Last year, a cyclist was struck by a truck on this street that has an unprotected bike lane.',
+            description: 'Overall, trucks were involved in half of last year’s 30 bike fatalities while over 1/3rd of cyclist accidents occured on bike marked streets. Last year, a cyclist was struck by a truck on this street that has an unprotected bike lane. Bike Lanes that are not protected provide no security from Trucks especially when the vehicles are making turns',
             location: {
                 center: [-73.95469, 40.68725],
                 zoom: 20.0,
@@ -94,12 +94,31 @@ var config = {
             id: 'fourth-chapter',
             alignment: 'fully',
             hidden: false,
-            title: 'Sharrows',
+            title: 'Sharrows are the Main Problem',
             image: 'images/bikedeath2.png',
-            description: 'Many of these accidents occur on "Sharrows" which are road markings indicating that cars and bicycles must share a lane. Two cyclists were killed last year riding in sharrows. Many argue that sharrows do nothing to protect cyclists and could also cause greater danger. This is because GPS apps will prefer to send riders down these streets instead of streets without any bike markings. However sharrows on truck routes have shown to be extremely dangerous. A rider was struck by a truck on this road last year by a box truck. ',
+            description: 'Many of these accidents occur on "sharrows" which are road markings indicating that cars and bicycles must share a lane. Two cyclists were killed last year riding in sharrows. Sharrows made up a large portion of the 400 miles of "bike lanes" created during the Bloomberg administration. Some argue that sharrows do nothing to protect cyclists and could also cause greater danger. This is because GPS apps will prefer to send riders down these streets instead of streets without any bike markings. A rider was struck by a truck on this road last year by a box truck. ',
             location: {
                 center: [-73.99335, 40.67265],
                 zoom: 15,
+                pitch: 0,
+                bearing: 0
+            },
+            mapAnimation: 'flyTo',
+            rotateAnimation: false,
+            callback: '',
+            onChapterEnter: [],
+            onChapterExit: []
+        },
+        {
+            id: 'fifth-chapter',
+            alignment: 'fully',
+            hidden: false,
+            title: 'Next Steps',
+            image: 'images/morebikelanes.jpg',
+            description: 'The City must keep upgrading sharrows with greater infrastructure protection for cyclists. Furthermore, GPS Apps should classify truck routes with unprotected bike lanes as a separate road category. This could allow for GPS Apps to program cyclist trips to avoid truck routes that do not have physical protected bike infrastructure.',
+            location: {
+                center: [-73.91737, 40.71908],
+                zoom: 11.5,
                 pitch: 0,
                 bearing: 0
             },
