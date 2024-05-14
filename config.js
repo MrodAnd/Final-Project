19d7d5a62a98e657900be0fb13eeb95294fc87de
MrodAnd <165328870+MrodAnd@users.noreply.github.com>
2024-05-14 10:36:36 -0400

    commit

--- config.js
+++ config.js
@@ -52,7 +52,7 @@ var config = {
             hidden: false,
             title: 'Streets with Protected Bike Lanes and Truck Routes are not the Problem',
             image: 'images/protectlane.jpg',
-            description: 'Some streets have both truck routes and bike lanes which is shown by the orange line. Truck routes that have protected bike lanes have shown not be a serious issue for cyclist safetly but truck routes that share the street with unprotected bike lanes and sharrows is a different story.',
+            description: 'Some streets have both truck routes and bike lanes which are shown by the orange line. Truck routes that have protected bike lanes are not dangerous for cyclists. However, truck routes that share streets with unprotected bike lanes and sharrows tell a different story.',
             location: {
                 center: [-73.91737, 40.71908],
                 zoom: 12.5,
@@ -77,7 +77,7 @@ var config = {
             hidden: false,
             title: 'Unprotected Bike Lanes can be an Issue',
             image: 'images/death1.jpg',
-            description: 'Overall, trucks were involved in half of last year’s 30 bike fatalities while over 1/3rd of cyclist accidents occured on bike marked streets. Last year, a cyclist was struck by a truck on this street that has an unprotected bike lane. Bike Lanes that are not protected provide no security from Trucks especially when the vehicles are making turns',
+            description: 'Overall, trucks were involved in half of last year’s 30 bike fatalities while over 1/3rd of cyclist accidents happened on bike marked streets. Last year, a cyclist was struck by a truck on this street below which has an unprotected bike lane. Bike Lanes that are unprotected provide little safety from trucks, especially when the vehicles are making turns.',
             location: {
                 center: [-73.95469, 40.68725],
                 zoom: 20.0,
@@ -96,7 +96,7 @@ var config = {
             hidden: false,
             title: 'Sharrows are the Main Problem',
             image: 'images/bikedeath2.png',
-            description: 'Many of these accidents occur on "sharrows" which are road markings indicating that cars and bicycles must share a lane. Two cyclists were killed last year riding in sharrows. Sharrows made up a large portion of the 400 miles of "bike lanes" created during the Bloomberg administration. Some argue that sharrows do nothing to protect cyclists and could also cause greater danger. This is because GPS apps will prefer to send riders down these streets instead of streets without any bike markings. A rider was struck by a truck on this road last year by a box truck. ',
+            description: 'Many of these accidents occur on "sharrows" which are road markings indicating that cars and bicycles must share a lane. Two cyclists were killed last year riding in sharrows. Sharrows made up a large portion of the 400 miles of "bike lanes" created during the Bloomberg administration. Some argue that sharrows do nothing to protect cyclists and could also cause greater danger. This is because GPS apps will prefer to send riders down these streets instead of streets without any bike markings. A cyclist was struck by a truck on this road last year by a box truck.',
             location: {
                 center: [-73.99335, 40.67265],
                 zoom: 15,
